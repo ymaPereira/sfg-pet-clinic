76e6cb346d8716eb44ca222229cb1dd02f710def
emanueld.pereira2@gmail.com <emanueld.pereira2@gmail.com>
2020-07-02 16:22:08 -0100

    Adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/petclinic/models/PetType.java
@@ -0,0 +1,16 @@
+package guru.springframework.petclinic.models;
+
+public class PetType {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
