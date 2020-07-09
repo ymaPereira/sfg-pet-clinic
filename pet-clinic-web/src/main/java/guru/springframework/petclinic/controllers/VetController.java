31e8c4c4ad7ab9c3a0f6d89e5e1612e65705191d
emanueld.pereira2@gmail.com <emanueld.pereira2@gmail.com>
2020-07-09 15:29:54 -0100

    List all Vets on Vet index Page. Closes #16

--- pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/VetController.java
@@ -1,15 +1,28 @@
 package guru.springframework.petclinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.petclinic.services.VetService;
 
 @RequestMapping("vets")
 @Controller
 public class VetController {
 
+	private final VetService vetService;
+	
+	@Autowired
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
+
 	@RequestMapping({"","/","/index","/index.html"})
-	public String listVets() {
-		
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 }
