035f707ad01116aceb3b55cc00d3a99b23b08800
emanueld.pereira2@gmail.com <emanueld.pereira2@gmail.com>
2020-07-09 14:11:45 -0100

    Create Home Index, Owner Index, Vet Index. #12 #11 #8 #7

---
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package guru.springframework.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("vets")
+@Controller
+public class VetController {
+
+	@RequestMapping({"","/","/index","/index.html"})
+	public String listVets() {
+		
+		return "vets/index";
+	}
+}
