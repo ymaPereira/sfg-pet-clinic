6acfa04e53029b226946ca04a742ebaebf81dac9
emanueld.pereira2@gmail.com <emanueld.pereira2@gmail.com>
2020-07-14 21:17:25 -0100

    Create PetType Map Service Impl. Closes #30

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.springframework.petclinic.services;
+
+import guru.springframework.petclinic.models.PetType;
+
+public interface PetTypeService extends CrudService<PetType, String>{
+
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,16 @@
+package guru.springframework.petclinic.services.map;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.petclinic.models.PetType;
+import guru.springframework.petclinic.services.PetTypeService;
+
+@Service
+public class PetTypeMapService extends AbstractServiceMap<PetType, String> implements PetTypeService{
+
+	@Override
+	public PetType save(PetType object) {
+		return super.save(null, object);
+	}
+
+}
