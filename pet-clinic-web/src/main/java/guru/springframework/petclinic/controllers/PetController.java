dd39999c549c93c95f49eb94cce1810ea87b0056
emanueld.pereira2@gmail.com <emanueld.pereira2@gmail.com>
2020-07-16 14:25:37 -0100

    Create Pet Index and Page Controller. Closes #14

---
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/PetController.java
@@ -0,0 +1,27 @@
+package guru.springframework.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.petclinic.services.PetService;
+
+@Controller
+@RequestMapping("/pet")
+public class PetController {
+
+	private final PetService petService;
+	
+	
+	public PetController(PetService petService) {
+		super();
+		this.petService = petService;
+	}
+
+
+	@RequestMapping({"","/","/index","/index.html"})
+	public String listVets(Model model) {
+		model.addAttribute("pets", petService.findAll());
+		return "pets/index";
+	}
+}
